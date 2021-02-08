86f9cfcfe9eed6da2ea94a4d5f1952e4b798e013
Chelle77322 <serenynadarra@gmail.com>
2021-02-09 10:00:28 +1030

    Adding the npm chalk and colour package to possibly use for assignment. Tidying up file structure

---
+++ assets/script/chalk.js
@@ -0,0 +1,8 @@
+const { blueBright } = require('chalk');
+const chalk = require('chalk');
+var color = require ('color');
+
+ 
+console.log(chalk.bgYellowBright , chalk.red('Hello world!'));
+color = color.red;
+console.log(color);
