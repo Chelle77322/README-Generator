117f54d4516ca8f64302c10da74a1079d60946fa
Chelle77322 <serenynadarra@gmail.com>
2021-02-09 12:33:25 +1030

    Uploading changes to the script files

--- assets/script/commander.js
+++ assets/script/commander.js
@@ -2,7 +2,21 @@
 
 const program = require('commander');
 const inquirer = require('inquirer');
-const opn = require('opn');
+const open = require('open');
 program.version('1.0.0')
     .description('ReadMe Generator')
     .parse(process.argv);
+
+    console.log(shalvah.bio);
+inquirer.prompt({
+    name: 'link',
+    type: 'list',
+    message: shalvah.prompt,
+    choices: shalvah.links.concat({
+        'name': `...Or shoot me an email (${shalvah.email})`,
+        'value': 'mailto:' + shalvah.email
+    })
+}).then(answers => {
+    console.log(`Opening ${answers.link}`);
+    opn(answers.link);
+});
---
+++ assets/script/example.js
@@ -0,0 +1,35 @@
+const shalvah = {
+    bio: "Hi, I'm Shalvah. I'm a backend software engineer based in Lagos, Nigeria. " +
+    "I currently work at Deimos (deimos.co.za) and guest-write for Pusher (pusher.com). " +
+    "I love building challenging APIs and automating stuff.",
+    prompt: 'Want to know more about me? Check me out on these sites:',
+    links: [
+        {
+            name: 'LinkedIn',
+            value: 'https://linkedin.com/in/theshalvah',
+        },
+        {
+            name: 'GitHub',
+            value: 'https://github.com/shalvah',
+        },
+        {
+            name: 'dev.to',
+            value: 'https://dev.to/shalvah',
+        },
+        {
+            name: 'Twitter',
+            value: 'https://twitter.com/theshalvah',
+        },
+        {
+            name: 'Medium',
+            value: 'https://medium/com/@shalvah',
+        },
+        {
+            name: 'StackOverflow',
+            value: 'https://stackoverflow.com/users/7370522/shalvah'
+        }
+    ],
+    email: 'hello@shalvah.me'
+};
+
+module.exports = shalvah;
